578017e2c04c61d05d6a8f5fc557b74c2c51ad5d
Puppy <puppyvenom@gmail.com>
2014-11-29 00:54:47 -0800

    mcmod.info

--- src/main/java/com/puppyvenom/firstmodtest/FirstModTest.java
+++ src/main/java/com/puppyvenom/firstmodtest/FirstModTest.java
@@ -1,9 +1,9 @@
 package com.puppyvenom.firstmodtest;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="FirstModTest", name="First Mod Test", version="1.7.2-1.0")
 public class FirstModTest
